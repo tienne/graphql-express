1102a649b6ef2dade8f07ca3913d03f652db3b4e
권윤 <yh.kwon@yellomobile.com>
2017-07-06 16:06:30 +0900

    passport 추가

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,17 +2,23 @@ import {Connection} from 'typeorm';
 import {getConnection} from './models';
 import {graphiqlExpress} from 'graphql-server-express';
 
-export default async function startServer (
-  {isDev = false, isTest = false}, dbConnection?: Connection
-) {
+/**
+ * 서버 실행 옵션
+ */
+export interface serverOption {
+  isDev : boolean
+  isTest : boolean
+}
+
+export default async function startServer(option:serverOption = {isDev:false, isTest:false}, dbConnection?: Connection) {
   const connection = dbConnection || await getConnection()
-  const app = await getServer(connection, isDev)
+  const app = await getServer(connection, option.isDev)
 
   // Adds Enviornment variables from .enviornment
-  const env = (isDev && 'development') || (isTest && 'test') || 'production'
+  const env = (option.isDev && 'development') || (option.isTest && 'test') || 'production'
 
   app.use('/graphql',ensureAuthenticated, buildGraphQLRouteHandler());
-  app.use('/graphiql',ensureAuthenticated, graphiqlExpress({endpointURL: '/graphql'}))
+  app.use('/graphiql',ensureAuthenticated, graphiqlExpress({endpointURL: '/graphql'}));
 
   return app
 }
