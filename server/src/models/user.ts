c8420d58cf7d6bc4a71497aefc31c90e4a0346ce
david <david@davidui-MacBook-Pro.local>
2017-07-07 18:37:41 +0900

    user model 작업중

---
+++ server/src/models/user.ts
@@ -0,0 +1,29 @@
+import 'reflect-metadata';
+import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+@Entity()
+export default class User {
+    @PrimaryGeneratedColumn()
+    public id?: number;
+
+    @Column()
+    public username: string;
+
+    @Column()
+    private password: string;
+    public setPassword = (pw: string) => this.password = pw;
+
+    /**
+     * 패스워드 유효성검사
+     * @param plainTextPassword
+     */
+    public validatePassword = async (plainTextPassword : string) => await bcrypt.compare(plainTextPassword, this.password + '');
+
+    public toJson = () => {
+        return {
+            id: this.id,
+            username: this.username
+        }
+    }
+}
