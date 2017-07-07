2d6f2354f2ca417750d0a91f58b03c2f728898a9
david <david@davidui-MacBook-Pro.local>
2017-07-07 18:11:18 +0900

    auth 처리 작업중

---
+++ server/src/auth.ts
@@ -0,0 +1,8 @@
+import * as passport from 'passport';
+import { Express, Response, Request } from 'express';
+import { Connection } from 'typeorm';
+
+
+export default async function setupPassport(server: Express, connection: Connection) {
+
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,37 @@
 import * as express from 'express';
 import * as session from 'express-session';
-import * as passport from 'passport';
-import * as flash from 'connect-flash';
-
-
+// import * as flash from 'connect-flash';
+import * as uuid from 'node-uuid';
 import * as bodyParser from 'body-parser';
-import { graphiqlExpress } from 'graphql-server-express';
+import * as connectRedis from 'connect-redis';
+import {Connection} from 'typeorm';
+import * as cors from 'cors';
 
-const PORT = 3000;
+const RedisStore = connectRedis(session);
 
-express()
-  .use('/graphql', bodyParser.json())
-  .use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
-  .listen(PORT);
+export default async function getServer(connection: Connection, isDev = false) {
+    let server = express();
+
+    if (isDev) {
+        const corsOptions = {
+            origin: (origin: string, cb: Function) => cb(null, true),
+            credentials: true
+        };
+        server.use(cors(corsOptions));
+    }
+
+    server.use(session({
+        secret: 'asdasdq',
+        genid: req => uuid.v4(),
+        store: new RedisStore({
+            host: '127.0.0.1',
+            port: 6379
+        })
+    })).use(bodyParser.urlencoded({
+        extends: true
+    })).use(bodyParser.json());
+
+
+    // server = await setupPass
+    return server;
+}
